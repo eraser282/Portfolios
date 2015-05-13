3e13029c0d54ef09bc164f6c449111bc3cb9cc8a
eraser282 <joel.colaco@bell.net>
2015-05-13 17:57:15 -0400

    Customizing Name

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace Portfolio.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Name = "Joel Colaco";
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
